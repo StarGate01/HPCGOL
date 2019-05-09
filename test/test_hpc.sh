956293f8ef45580bd3f930b8b4e47373a3e6b1ea
Christoph Honal <christoph.honal@web.de>
2019-05-09 23:49:38 +0200

    results

--- test/test_hpc.sh
+++ test/test_hpc.sh
@@ -2,7 +2,7 @@
 #SBATCH -o /home/hpc/pr63so/ga63yos2/HPCGOL/test/out/job.%j.out
 #SBATCH -e /home/hpc/pr63so/ga63yos2/HPCGOL/test/out/job.%j.err
 #SBATCH -D /home/hpc/pr63so/ga63yos2/HPCGOL
-#SBATCH -J hpcgol_fat4
+#SBATCH -J hpcgol_fat2
 #SBATCH --mail-user=christoph.honal@tum.de
 #SBATCH --mail-type=ALL
 #SBATCH --clusters=mpp2
@@ -19,7 +19,7 @@ export OMP_NUM_THREADS=28
 NODES=16
 EXE=hybrid
 STEPS=10
-WIDTH=40000
-HEIGHT=40000
+WIDTH=10000
+HEIGHT=10000
 
 make run EXE=$EXE ARG_PRINT=0 ARG_STEPS=$STEPS ARG_WIDTH=$WIDTH ARG_HEIGHT=$HEIGHT ARG_THREADS=28 ARG_NODES=$NODES
