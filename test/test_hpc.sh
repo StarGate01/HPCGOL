dda5535c08a0c1da39fa6682d3a2c62c802b100d
Christoph Honal <christoph.honal@web.de>
2019-05-09 22:21:14 +0200

    adjust tests

--- test/test_hpc.sh
+++ test/test_hpc.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-#SBATCH -o /home/hpc/pr63so/ga63yos2/HPCGOL/test/job.%j.out
-#SBATCH -e /home/hpc/pr63so/ga63yos2/HPCGOL/test/job.%j.err
+#SBATCH -o /home/hpc/pr63so/ga63yos2/HPCGOL/test/out/job.%j.out
+#SBATCH -e /home/hpc/pr63so/ga63yos2/HPCGOL/test/out/job.%j.err
 #SBATCH -D /home/hpc/pr63so/ga63yos2/HPCGOL
-#SBATCH -J hpcgol
+#SBATCH -J hpcgol_fat
 #SBATCH --mail-user=christoph.honal@tum.de
 #SBATCH --mail-type=ALL
 #SBATCH --clusters=mpp2
 #SBATCH --get-user-env
-#SBATCH --ntasks=1
+#SBATCH --ntasks=24
 #SBATCH --cpus-per-task=28
-#SBATCH --time=00:01:00
+#SBATCH --time=00:05:00
 
 source /etc/profile.d/modules.sh
 
 export LC_NUMERIC="en_US.UTF-8"
 export OMP_NUM_THREADS=28
 
-NODES=1
+NODES=24
 EXE=hybrid
 STEPS=10
 WIDTH=50000
